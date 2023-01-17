f6043a3a263b43655cc1bcd292c7cab6d2f21a00
PepperoniP1zza <i95840715@gmail.com>
2023-01-17 12:28:32 +0900

    공지사항 엔티티, 레파지토리

---
+++ Back/spring-boot/src/main/java/com/project/irunyou/data/repository/NoticeBoardRepository.java
@@ -0,0 +1,14 @@
+/* 작성자 : 홍지혜
+ * 파일의 역할 : 공지사항 Repository
+ * 작성날짜 : 2023-01-17
+ * 
+ * 업데이트 작성자 : -
+ * 업데이트 날짜 : -
+ * */
+package com.project.irunyou.data.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeBoardRepository extends JpaRepository<NoticeBoardRepository,Integer>{
+
+}
