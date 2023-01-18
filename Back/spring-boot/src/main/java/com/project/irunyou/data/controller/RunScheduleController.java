31932698689ee80f9ff6910aa133af6da8a08585
PepperoniP1zza <i95840715@gmail.com>
2023-01-18 10:32:43 +0900

    일정 수정, 삭제

--- Back/spring-boot/src/main/java/com/project/irunyou/data/controller/RunScheduleController.java
+++ Back/spring-boot/src/main/java/com/project/irunyou/data/controller/RunScheduleController.java
@@ -5,44 +5,62 @@
  * 업데이트 작성자 : 홍지혜
  * 업데이트 날짜 : 2023-01-17
  * 업데이트 내용 : 일정 등록, 일정 조회 컨트롤러
+ * 
+ * 업데이트 작성자 : 홍지혜
+ * 업데이트 날짜 : 2023-01-18
+ * 업데이트 내용 : 일정 수정, 삭제 컨트롤러
  * */
 package com.project.irunyou.data.controller;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.irunyou.data.dto.FindRunScheduleDto;
 import com.project.irunyou.data.dto.GetUserRunScheduleDto;
+import com.project.irunyou.data.dto.PatchScheduleDto;
 import com.project.irunyou.data.dto.ResponseDto;
 import com.project.irunyou.data.dto.ResultResponseDto;
 import com.project.irunyou.data.dto.RunScheduleDto;
+import com.project.irunyou.data.dto.UserRequestDto;
+import com.project.irunyou.data.repository.UserRepository;
 import com.project.irunyou.data.service.RunScheduleService;
 
 @RestController
-@RequestMapping("irunyou/")
+@RequestMapping("irunyou/shedule/")
 public class RunScheduleController {
 
 	@Autowired RunScheduleService scheduleService;
 	
 	//C (일정등록)
-	@PostMapping("runschedule")
+	@PostMapping("schedule_regist")
 	public ResponseDto<ResultResponseDto> registSchedule(@RequestBody RunScheduleDto requestBody) {
 		return scheduleService.registSchedule(requestBody);
 	}
 	//R (일정조회)
-	@GetMapping("runschedule/{email}")
-	public ResponseDto<List<GetUserRunScheduleDto>> readSchedule(@PathVariable String email) {
-		return scheduleService.readSchedule(email);
+	@GetMapping("schedule_info")
+	public ResponseDto<List<GetUserRunScheduleDto>> readSchedule(@RequestBody UserRequestDto dto) {
+		return scheduleService.readSchedule(dto);
 	}
 	
 	//U
-	
-	//D
+	@PatchMapping("")
+	public ResponseDto<GetUserRunScheduleDto> patchSchedule(@RequestBody PatchScheduleDto dto) {
+		return scheduleService.patchSchedule(dto);
+	}
+		
+	//D (일정삭제)
+	@DeleteMapping("")
+	public ResponseDto<ResultResponseDto> deleteSchedule(@RequestBody FindRunScheduleDto dto) {
+		return scheduleService.deleteSchedule(dto);
+	}
 	
 }
---
+++ Back/spring-boot/src/main/java/com/project/irunyou/data/dto/FindRunScheduleDto.java
@@ -0,0 +1,16 @@
+/* 작성자 : 홍지혜
+ * 파일의 역할 : 스케줄 삭제용 reqeust dto
+ * 작성날짜 : 2023-01-18
+ */
+package com.project.irunyou.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FindRunScheduleDto {
+	private int sch_idx;
+}
