222b18a7cfff4fac1c97bdeadf51527475b90cd4
PepperoniP1zza <i95840715@gmail.com>
2023-01-31 12:40:44 +0900

    일반 로그인 localstorage jwt토큰 발급 구현

--- Front/irunyou/src/components/Login/Login.tsx
+++ Front/irunyou/src/components/Login/Login.tsx
@@ -3,6 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import kakao from '../../assets/images/kakao_login_medium_wide.png';
 import { useState } from "react";
 import axios from "axios";
+import { LoginAction } from "../../service/auth-service";
+import { usePreviousProps } from "@mui/utils";
 const { Kakao }:Window = window;
 
 // 작성자 : 최예정
@@ -13,28 +15,32 @@ const { Kakao }:Window = window;
 // 업데이트 날짜 : 2023-01-30
 
 
-export default function Login() {
 
-
-    const [id, setId] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
+export default function Index() {
+    
+    const [userEmail, setUserEmail] = useState<string>('');
+    const [userPassword, setUserPassword] = useState<string>('');
+    
     const movePage = useNavigate();
+    
+    const loginButton = () => {
+        
+        LoginAction(userEmail,userPassword, movePage);
 
-    const LoginHandler = () => {
-        const data = {
-            id,
-            password
-        }
+        // const data = {
+        //     id,
+        //     password
+        // }
 
-        axios.post('http://localhost:4040/irunyou/auth/login', data).then((Response) => {
-            const UserInformation = Response.data.user;
-            console.log(UserInformation);
+        // axios.post('http://localhost:4040/irunyou/auth/login', data).then((Response) => {
+        //     const UserInformation = Response.data.user;
+        //     console.log(UserInformation);
 
-            if(!UserInformation) alert('입력하신 회원정보가 존재하지않습니다.');
-            else movePage('/MainPage');
-        })
-        // 로그인을 할 시 사용자가 입력한 데이터가 일치하지 않을 경우 경고창 띄우기
-        // 지금 적어놓은 조건문은 else에 있는 조건문만 실행됨
+        //     if(!UserInformation) alert('입력하신 회원정보가 존재하지않습니다.');
+        //     else movePage('/MainPage');
+        // })
+        // // 로그인을 할 시 사용자가 입력한 데이터가 일치하지 않을 경우 경고창 띄우기
+        // // 지금 적어놓은 조건문은 else에 있는 조건문만 실행됨
     } 
 
     const loginWithKakao = () => {
@@ -44,18 +50,17 @@ export default function Login() {
             scope : "profile_nickname, account_email",
         });
     };
-    
 
     return(
         <div className="login-container">
             {/* 로그인 전체 form */}
-            <form id="input-form" className="form-contant">
+            <div id="input-form" className="form-contant">
                     <h1 className="login-form">LOGIN</h1>
                 {/* 아이디(이메일), 비밀번호 입력창 */}
                 <fieldset>
                     <div className="input-border">
-                        <input className="email-input login-input" type="text" placeholder="아이디(이메일)" required />
-                        <input className="password-input login-input" type="password" placeholder="비밀번호(8~16자 숫자, 영문, 특수문자)" required />
+                        <input className="email-input login-input" type="text" placeholder="아이디(이메일)" required onChange={(e) => setUserEmail(e.target.value)}/>
+                        <input className="password-input login-input" type="password" placeholder="비밀번호(8~16자 숫자, 영문, 특수문자)" required onChange={(e) => setUserPassword(e.target.value)}/>
                     </div>
                 </fieldset>
                 <div className="box">
@@ -77,14 +82,14 @@ export default function Login() {
                 </div>
                 {/* 로그인, 회원가입 각 버튼 */}
                 <div className="login-signup-button">
-                    <Link to="/MainPage">
-                    <button className="login-button button" onClick={() => LoginHandler()}>로그인</button>
-                    </Link>
+                    {/* <Link to="/MainPage"> */}
+                    <button type="button" className="login-button button" onClick={() => loginButton()}>로그인</button>
+                    {/* </Link> */}
                     <Link to="/Signup">
                         <button className="signup-button button" >회원가입</button>
                     </Link>
                 </div>
-            </form>
+            </div>
         </div>
     );
 }
---
+++ Front/irunyou/src/service/auth-service.tsx
@@ -0,0 +1,75 @@
+import axios from "axios";
+import { create } from "domain";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
+const createTokenHeader = (token: string) => {
+  return {
+    headers: {
+      Authorization: "Bearer " + token
+    }
+  };
+};
+
+const calculateRemainingTime = (expiration: number) => {
+  const currentTime = new Date().getTime();
+  const adjExpirationTime = new Date(expiration).getTime();
+  const remainingDuration = adjExpirationTime - currentTime;
+
+  return remainingDuration;
+};
+
+export const loginTokenHandler = (token: string, expiration: number) => {
+  localStorage.setItem("token", token);
+  localStorage.setItem("expirationTime", String(expiration));
+
+  const remainingTime = calculateRemainingTime(expiration);
+
+  return remainingTime;
+};
+
+export const retrieveStoredToken = () => {
+  const storedToken = localStorage.getItem("token");
+  const storedExpirationDate = localStorage.getItem("expirationTime") || "0";
+  const remaingTime = calculateRemainingTime(+storedExpirationDate);
+
+  if (remaingTime <= 1000) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("expirationTime");
+
+    return null;
+  }
+
+  return {
+    token: storedToken,
+    duration: remaingTime
+  };
+};
+
+export const LoginAction = (
+  userEmail: string,
+  userPassword: string,
+  movePage: any
+) => {
+  return axios
+    .post("http://localhost:4040/irunyou/auth/login", {
+      userEmail,
+      userPassword
+    })
+    .then((response) => {
+      const tokendata = response.data;
+
+      if (!tokendata.result) {
+        alert("token error");
+        return;
+      }
+
+      loginTokenHandler(tokendata.token, tokendata.expiration);
+      alert("로그인 되었습니다.");
+      movePage("/MainPage");
+    })
+    .catch((error) => {
+      alert(error.response.data);
+      movePage("/Login");
+    });
+};
