633dd0fc4a811c5818c861999d90b15cbc60b560
PepperoniP1zza <i95840715@gmail.com>
2023-02-03 21:27:34 +0900

    cors 에러 해결

--- Back/spring-boot/src/main/java/com/project/irunyou/IrunyouApplication.java
+++ Back/spring-boot/src/main/java/com/project/irunyou/IrunyouApplication.java
@@ -11,6 +11,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableJpaAuditing
@@ -27,8 +29,11 @@ public class IrunyouApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOriginPatterns("http://localhost:3000").allowedMethods("GET", "POST", "PUT", "DELETE")
-				.allowedHeaders("*").exposedHeaders("*");
+				registry.addMapping("/**")
+					.allowedMethods(CorsConfiguration.ALL)
+					.allowedHeaders(CorsConfiguration.ALL)
+					.allowedOriginPatterns(CorsConfiguration.ALL);
+				
 			}
 		};
 	}
