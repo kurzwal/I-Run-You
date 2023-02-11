b17224f31e42f82bd08ca1633b66fd958b2fe89c
PepperoniP1zza <i95840715@gmail.com>
2023-02-12 08:08:20 +0900

    프론트-백 댓글 리스트 연결

--- Front/irunyou/src/components/MenuComp/Parkinfo/CommentItem.tsx
+++ Front/irunyou/src/components/MenuComp/Parkinfo/CommentItem.tsx
@@ -1,8 +1,10 @@
+import "./Comment.css";
+
 interface Props {
     comment: {
         commentIndex: number;
         commentScheduleIndex: number;
-        commentWriterIndex: number;
+        commentWriter: string;
         commentContent: string;
         commentDatetime: string;
     }
@@ -13,8 +15,17 @@ interface Props {
 //  망했다
 
 export default function CommentItem({ comment }: Props) {
+    const date = comment.commentDatetime.substring(0, 10);
+    const time = comment.commentDatetime.substring(11, 19);
+
     return (
         <div className="comment-item-wraper">
+            <div>{comment.commentWriter}</div>
+            <div className="comment-content-container">{comment.commentContent}</div>
+            <div className="comment-time-container">
+                <div>{date}</div>
+                <div>{time}</div>
+            </div>
         </div>
     )
 }
