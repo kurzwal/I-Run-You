e740f2655eaeb375a45f9db24566c5eed4f797b5
kyeongwonMoon <mkw1999@naver.com>
2023-02-13 10:04:43 +0900

    정보수정시 비밀번호 확인

--- Front/irunyou/src/components/MenuComp/Myinfo/MyInfoVerify.tsx
+++ Front/irunyou/src/components/MenuComp/Myinfo/MyInfoVerify.tsx
@@ -1,10 +1,15 @@
 import React, { Dispatch, SetStateAction } from 'react'
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 
 import './MyInfoVerify.css';
 
 import TextField from '@mui/material/TextField';
 import { createTheme, ThemeProvider } from '@mui/material';
+
+import { useEffect, useState } from "react";
+
+import axiosInstance from "../../../service/axiosInstance";
 
 interface Props {
     setModal: Dispatch<SetStateAction<boolean>>;
@@ -18,7 +23,46 @@ const theme:any = createTheme({
     }
 })
 
+
+
 export default function MyInfoVerify({setModal}: Props) {
+
+    const movePage = useNavigate();
+
+const [userEmail, setUserEmail] = useState<string>('');
+
+useEffect(()=> {
+    axiosInstance.get('http://localhost:4040/irunyou/mypage').then((response) => {
+        setUserEmail(response.data.data.userEmail);
+    })
+},[])
+
+const [userPassword, setUserPassword] = useState<string>('');
+
+const loginAction = () => {
+
+    console.log(userEmail);
+
+    return axios.post("http://localhost:4040/auth/login", {
+        userEmail,
+        userPassword
+    }).then((response) => {
+        
+            const responseMessage = response.data.message;
+
+            if (response.data.status) {
+                movePage("/InfoModify");
+
+            } else {
+                alert('비밀번호를 확인하세요');
+            }
+
+        }).catch((error) => {
+            alert(error.message);
+        })
+
+}
+
   return (
     <div id="info-modal" className="info-modal-overlay">
         <div className="info-modal-window">
@@ -27,15 +71,13 @@ export default function MyInfoVerify({setModal}: Props) {
             </div>
             <div className="content">
             <ThemeProvider theme={theme}>
-                <TextField size='small' label="비밀번호 확인" type="password" sx={{width: "100%"}}  id="outlined-basic" variant="outlined" margin="normal" required/>
+                <TextField size='small' label="비밀번호 확인" type="password" sx={{width: "100%"}}  id="outlined-basic" variant="outlined" margin="normal" required onChange={(event) => setUserPassword(event.target.value)}/>
                     {/* <input className='singup-input' onChange={(e) => setPassword(e.target.value)} type="password" placeholder="비밀번호(8~16자까지 영문 대소문자/숫자/특수문자 사용)" /> */}
             </ThemeProvider>
             </div>
             <br />
             <div className="modal-btn">
-                <Link to="/InfoModify">
-                <button className="ok-btn submit-btn">확인</button>
-                </Link>
+                <button className="ok-btn submit-btn" onClick={() => loginAction()}>확인</button>
                 <button className="submit-btn" onClick={() => setModal(false)}>취소</button>
             </div>
         </div>
